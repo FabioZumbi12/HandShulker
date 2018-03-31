dba3d3252a948837c79f2befa75793bd39020f75
FabioZumbi12 <zumbi12doze@gmail.com>
2018-03-31 12:47:51 -0300

    Base libs.

---
+++ Sponge78/src/main/java/br/net/fabiozumbi12/HandShulker/Sponge/HandShulker.java
@@ -0,0 +1,11 @@
+package br.net.fabiozumbi12.HandShulker.Sponge;
+
+import org.spongepowered.api.plugin.Plugin;
+
+@Plugin(id = "handshulker",
+        name = "HandShulker",
+        version = VersionData.VERSION,
+        authors="FabioZumbi12",
+        description="Open your shulker boxes in your hand")
+public class HandShulker {
+}
